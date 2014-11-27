5ce4934b156b619b8d0653312d0fa65e4cd42116
jonschlinkert <dev@sellside.com>
2014-11-27 02:36:15 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * examples <https://github.com/jonschlinkert/examples>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
